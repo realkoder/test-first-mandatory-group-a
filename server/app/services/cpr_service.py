faaede36cb312e27165b577909191a650b10325a
Davijo <danvj93@gmail.com>
2025-10-22 13:48:10 +0200

    Create DOB func works

--- server/app/services/cpr_service.py
+++ server/app/services/cpr_service.py
@@ -6,32 +6,36 @@ from app.repository.repository import get_person
 
 
 # Kirsten Schwalbe's DOB.
-epoch = datetime.datetime(1914, 3, 10,0,00,00,00)
+epoch = datetime.date(1914, 3, 10)
 
 def generate_dob(epoch):
-    today = datetime.datetime.now()
+    today = datetime.date.today()
 
     delta = today - epoch
     delta_in_days = delta.days
-    rand_days = random.randrange(0, delta_in_days)
 
+    rand_days = random.randint(0, delta_in_days)
+    dob = epoch + datetime.timedelta(days=rand_days)
 
-    return today, epoch, delta, delta_in_days, rand_days
+    return dob
 
 async def get_random_cpr_number():
     person = get_person()
+    
+    # DOB and first 6 CPR digits
+    dob = generate_dob(epoch)
 
     # Last digit based on gender + 3x random nums
     if person["gender"] == "female":
         last_digit = random.choices([0,2,4,6,8])
     else:
         last_digit = random.choices([1,3,5,7,9])
+        
     three_digits = random.randrange(0,999)
     three_digits_str = f"{three_digits:03d}"
 
     four_digits = f"{three_digits:03d}{last_digit}"
 
-    dob = generate_dob(epoch)
 
     return {
         "first_name": person["name"],
@@ -41,9 +45,5 @@ async def get_random_cpr_number():
         "three_digits": three_digits,
         "three_digits_str": three_digits_str,
         "four_digits": four_digits,
-        "today": dob[0],
-        "epoch": dob[1],
-        "delta": dob[2],
-        "delta_in_days": dob[3],
-        "rand_days": dob[4],
+        "dob": dob,
     }
